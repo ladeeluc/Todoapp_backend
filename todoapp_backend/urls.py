ef9b899182f450e89635c07391ccf7c921d8facc
Ladeeluc <laura.hatch.lh@gmail.com>
2021-08-11 22:50:45 -0400

    set up enviornment

--- todoapp_backend/urls.py
+++ todoapp_backend/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from todoapp_backend_app.views import index_view
 from django.contrib import admin
 from django.urls import path
+from todoapp_backend_app.views import index_view
 
 urlpatterns = [
+    path('', index_view),
     path('admin/', admin.site.urls),
 ]
